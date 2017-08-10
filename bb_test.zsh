560952dee675fa2bdea83baf944a85ad77dd45d1
root <erm2718@gmail.com>
2017-08-10 16:32:42 -0400

    added .zsh for completeness

---
+++ bb_test.zsh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+function BB_test(){
+	#local count=0; for more or less
+	local bb_5="$(sudo smartctl -A /dev/${1} | awk '/5 Reallocated_Sector_Ct/' | awk '{print substr($0,85,6)}')";
+	local bb_187="$(sudo smartctl -A /dev/${1} | awk '/187 Reported_Uncorrect/' | awk '{print substr($0,85,6)}')";
+	local bb_188="$(sudo smartctl -A /dev/${1} | awk '/188 Command_Timeout/'| awk '{ print substr($0,85,6)}')";
+	local bb_197="$(sudo smartctl -A /dev/${1} | awk '/197 Current_Pending_Sector/'| awk '{ print substr($0,85,6)}')";
+	local bb_198="$(sudo smartctl -A /dev/${1} | awk '/198 Offline_Uncorrectable/' | awk '{ print substr($0,85,6)}')";
+	#bb_5="500";
+	if ((bb_5==0))&&((bb_187==0))&&((bb_188==0))&&((bb_197==0))&&((bb_198==0)) ;then
+		#pass
+		echo $bb_5;
+		echo $bb_187;
+		echo $bb_188;
+		echo $bb_197;
+		echo $bb_198;
+		echo pass;
+	else
+		#fail
+		echo fail;
+	fi
+}
+main()
+{
+	echo ${1};	
+	BB_test ${1} ;
+	echo here;
+}
+main $1;
