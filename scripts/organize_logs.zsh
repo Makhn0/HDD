943a858ecdeb0ab7a88582028f2766368770c222
Makhn0 <erm2718@gmail.com>
2018-01-26 16:30:14 -0500

    organize_logs prints to stdout

--- scripts/organize_logs.zsh
+++ scripts/organize_logs.zsh
@@ -1,14 +1,16 @@
 #!/bin/bash
 function createMaster(){
-	Count=1;
+
 	server=/home/hdd-test-server
 	outfile=$server/nwipe_2018/master.csv
 	echo $outfile
 	if [[ -e $outfile ]]; then
 		echo outfile=$outfile
 	else
-		touch $outfile
+		echo "creating outputfile at $outfile"
+		sudo touch $outfile
 	fi
+	Count=1;
 	while (( $Count != 51 )); do
 		>&2 echo $Count;
 		client=/home/test$Count
@@ -17,10 +19,11 @@ function createMaster(){
 		
 		if [[ -e $clientLog ]]; then
 			./add_master.zsh $clientLog #>> $outfile
-			cat $clientLog >> $ndir/archive #move log contents to archive folder so successive calls to this function don't add the same data
-			echo "" > $clientLog #erasesLog file
+			#test above code before uncommenting below
+			#cat $clientLog >> $ndir/archive #move log contents to archive folder so successive calls to this function don't add the same data
+			#echo "" > $clientLog #erasesLog file
 		else
-			echo "nwipe_2018/Log file for $client"
+			echo "no nwipe_2018/Log file for $client"
 		fi
 		(( Count = $Count + 1 ));
 	done;		
