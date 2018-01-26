638f759c62116ab7c3d636ffc93cfc355b33e454
Makhn0 <erm2718@gmail.com>
2018-01-26 12:45:11 -0500

    debug n_to_g.zsh

--- scripts/n_to_g.zsh
+++ scripts/n_to_g.zsh
@@ -16,40 +16,24 @@ function find_dev(){
 	A_pass1=
 	
 }
-function find_nm(){
-	########################parses nwipe's output log file and puts it into a more readable and searchable csv format
-	N=${2}
-	M=${3}
-	fname=/dev/${1} 
-	#echo fname=$fname
-	sudo cat > temp
+function find_erase_between(){
+	#inputs are line number of target drive(n), line number of next drive(m) and file name(fname) ex. sda
+	#outputs passed if it can find lines verifying  erasure and reporting "blanked device" lines in between n, and m, otherwise outputs not passed
 
-	client= $(pwd |grep -oP "test\d{1,2}")
-	#client=$(pwd | grep -oP -e "scrip.{1,2}$")
-	A=$(cat -n temp | grep "nwipe: info: Device $fname has serial number" | sed -n ${N},${N}p |grep -o "\S*$" )
-	A_n=$(cat -n temp | grep "nwipe: info: Device $fname has serial number"| sed -n ${N},${N}p | grep -oP "^\s*\d*\s" | grep -oP "\d*" )
-	A_next_n=$(cat -n temp | grep "nwipe: info: Device $fname has serial number"| sed -n $((N+1)),$((N+1))p | grep -oP "^\s*\d*\s" | grep -oP "\d*" )
-
-	#pass1_n=2
-
-	if [[ -z $A ]]
-	then
-		return;
+	fname=${1}
+	n=${2}
+	m=${3}
+	if [[ -z $m ]]; then
+		m='$'
 	fi
-	As=$(cat -n temp | grep "nwipe: info: Device '$fname' is size" | sed -n ${N},${N}p | grep -oP "\s\d*.$" | grep -oP "\d*")
-	As_n=$(cat -n temp | grep "nwipe: info: Device '$fname' is size" |sed -n ${N},${N}p | grep -oP "^\s*\d*" | grep -oP "\d*" )
-
-	pass0=$(cat temp | grep "nwipe: notice: Verified that '$fname' is empty."|sed -n ${M},${M}p)
-	pass1=$(cat -n temp | grep "nwipe: notice: Blanked device '$fname'." | sed -n ${M},${M}p)
-	pass1_n=$(cat -n temp | grep "nwipe: notice: Blanked device '$fname'." | sed -n ${M},${M}p | grep -oP "^\s*\d*" | grep -oP "\d*")
-	#echo A_next_n=$A_next_n
-	#echo pass1_n=$pass1_n
-	if [[ $A_next_n ]] ;
-	then 
-		if (( $A_next_n < $pass1_n )) ; then
-			return;
-		fi
-	fi
+	sedcmd="${n},${m}p"
+	#>&2 echo here
+	#>&2 echo sedcmd=$sedcmd
+	pass0=$(cat -n temp | sed -n $sedcmd | grep "nwipe: notice: Verified that '$fname' is empty.")
+	#>&2 echo pass0=$pass0
+	pass1=$(cat -n temp | sed -n $sedcmd | grep "nwipe: notice: Blanked device '$fname'."  )
+	#>&2 echo pass1=$pass1
+	#pass1_n=$(cat -n temp | sed -n ${M},${m}p|grep "nwipe: notice: Blanked device '$fname'."  | grep -oP "^\s*\d*" | grep -oP "\d*")
 	if [[ -z $pass0 ]] || [[ -z $pass1 ]]
 	then 
 
@@ -57,23 +41,48 @@ function find_nm(){
 	else
 		pass="passed"
 	fi
+
+	echo $pass
+}
+function find_n(){
+	########################parses nwipe's output log file and puts it into a more readable and searchable csv format
+	#arguments are file namd ex. sda, N'th erasure
+	## intended to be piped into by cat logfile
+	N=${2}
+	
+	fname=/dev/${1} 
+	#>&2 echo fname=$fname
+	sudo cat > temp
+
+	client= $(pwd |grep -oP "test\d{1,2}")
+	#client=$(pwd | grep -oP -e "scrip.{1,2}$")
+	#gets serial number from stdout
+	A=$(cat -n temp | grep "nwipe: info: Device $fname has serial number" | sed -n ${N},${N}p |grep -o "\S*$" )
+	
+	A_n=$(cat -n temp | grep "nwipe: info: Device $fname has serial number"| sed -n ${N},${N}p | grep -oP "^\s*\d*\s" | grep -oP "\d*" )
+	#gets line number of next drive in put into sda along with
+	A_next_n=$(cat -n temp | grep "nwipe: info: Device $fname has serial number"| sed -n $((N+1)),$((N+1))p | grep -oP "^\s*\d*\s" | grep -oP "\d*" )
+	
+	#>&2 echo A=$A	
+	#>&2 echo n=$A_n
+	#>&2 echo m=$A_next_n
+
+	if [[ -z $A ]]
+	then
+		return; #returns meaning it outputs nill, which signals to calling function to terminate
+	fi
+	#this is bad
+	#As=$(cat -n temp | grep "nwipe: info: Device '$fname' is size" | sed -n ${N},${N}p | grep -oP "\s\d*.$" | grep -oP "\d*")
+	#As_n=$(cat -n temp | grep "nwipe: info: Device '$fname' is size" |sed -n ${N},${N}p | grep -oP "^\s*\d*" | grep -oP "\d*" )
+
+	#print "AS="$As
+	pass=$(find_erase_between $fname $A_n $A_next_n)
 	echo $client , $A, $As, $pass
 
 
 }
 
-function find_n(){
-	N=${2}
-	M=${3}
-	line=$(find_nm ${1} $N $M)
-	if [[ $line ]]; then
-	echo $line
-	fi
 
-
-
-}
-
-find_n ${1} ${2} ${3}
+find_n ${1} ${2}
 
 
