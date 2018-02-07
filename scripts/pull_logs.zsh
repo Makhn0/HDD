27cb909380b519e55fffb44788d52858be918f5f
Makhn0 <erm2718@gmail.com>
2018-02-07 13:16:11 -0500

    make pull_logs need explicit arguments

--- scripts/pull_logs.zsh
+++ scripts/pull_logs.zsh
@@ -1,18 +1,13 @@
 #!/bin/bash
-function createMaster(){
-	#retrieves all of the log data from all the clients' /nwipe_2018/log files, formats them with ./add_master redirects output to master.csv
-	server=/home/hdd-test-server
-	outfile=$server/nwipe_2018/master.csv
-	>&2 echo outfile=$outfile
-	if [[ -e $outfile ]]; then
-		>&2 echo "outfile already exists"
-	else
-		>&2 echo "creating outputfile at $outfile"
-		touch $outfile
-	fi
-	Count=1;
-	while (( $Count != 51 )); do
-		>&2 echo $Count;
+server=/home/hdd-test-server
+outfile=$server/nwipe_2018/master.csv
+function print_noargs(){
+	printf "tell me where to pull from \n"	
+	}
+function extract_from(){
+	#takes client number and extracts /home/test#/nwipe_2018/Log to master
+	count=${1}
+	>&2 echo $Count;
 		client=/home/test$Count
 		ndir=$client/nwipe_2018
 		clientLog=$ndir/Log
@@ -25,12 +20,37 @@ function createMaster(){
 		else
 			>&2 echo "no nwipe_2018/Log file for $client"
 		fi
-		(( Count = $Count + 1 ));
+	
+}
+function createMaster_all(){
+	#retrieves all of the log data from all the clients' /nwipe_2018/log files, formats them with ./add_master redirects output to master.csv
+
+	>&2 echo outfile=$outfile
+	if [[ -e $outfile ]]; then
+		>&2 echo "outfile already exists"
+	else
+		>&2 echo "creating outputfile at $outfile"
+		touch $outfile
+	fi
+	Count=1;
+	while (( $Count != 51 )); do
+		extract_from $Count
+		Count=$(($Count+1))
 	done;	
 	>&2 printf "%s\n" "finished creating $outfile"
 }
-
-createMaster 
+function createMaster(){
+	if [[ ${1} == '--all' ]]; then
+		createMaster_all
+	
+	elif [[ ${1} == '' ]]; then
+		print_noargs
+	else
+		extract_from ${1}
+	fi
+	
+}
+createMaster ${1}
 
 ##add to .bashrc later find way to load on clients
 #the idea is to have standard commands for testing erasing etc. 
